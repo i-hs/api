99e4778c852a00a5b797c61b3db692501030f0ef
lavistar <wila@tistory.com>
2020-01-11 17:38:48 +0900

    create follow/unfollow

--- app.py
+++ app.py
@@ -1,58 +1,77 @@
 from flask import Flask, request, jsonify
+from flask.json import JSONEncoder
 
-app             =   Flask(__name__)
-app.id_count    =   1
-app.users       =   {}
-app.tweet      =   []
-app.follow      =   []
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+        return JSONEncoder.default(self, obj)
+
+
+app = Flask(__name__)
+app.id_count = 1
+app.users = {}
+app.tweet = []
+app.follow = []
+app.json_encoder = CustomJSONEncoder
+
 
 @app.route("/ping", methods=['GET'])
 def ping():
     return "pong"
 
+
 @app.route("/sign-up", methods=['POST'])
 def sign_up():
-    new_user            =   request.json
-    new_user["id"]      =   app.id_count
-    app.users[app.id_count] =   new_user
-    app.id_count            +=  1
+    new_user = request.json
+    new_user["id"] = app.id_count
+    app.users[app.id_count] = new_user
+    app.id_count += 1
 
     return jsonify(new_user)
+
 
 @app.route('/tweet', methods=['POST'])
 def tweet():
     payload = request.json
     user_id = int(payload['id'])
-    tweet   = payload['tweet']
+    tweet = payload['tweet']
 
     if user_id not in app.users:
         return '사용자가 존재하지 않습니다.', 400
 
-    if len(tweet)   >   300:
+    if len(tweet) > 300:
         return '300자를 초과했습니다.', 400
+    user_id = int(payload['id'])
 
     app.tweet.append({
-        'user_id' : user_id,
-        'tweet'   : tweet
-        })
+        'user_id': user_id,
+        'tweet': tweet
+    })
     return '', 200
+
 
 @app.route('/follow', methods=['POST'])
 def follow():
-    
     payload = request.json
     user_id = int(payload['id'])
-    
-    if user_id not in app.users:
+    user_id_to_follow = int(payload['follow'])
+
+    if user_id not in app.users or user_id_to_follow not in app.users:
         return '사용자가 존재하지 않습니다.', 400
-    follow = payload['follow'] 
-    if follow not in app.follows:
-        app.users[user_id]['follow'].append(payload)
+    user = app.users[user_id]
+    user.setdefault('follow', set()).add(user_id_to_follow)
+    return jsonify(user)
 
 
 @app.route('/unfollow', methods=['POST'])
 def unfollow():
-    
     payload = request.json
     user_id = int(payload['id'])
-    app.users[user_id]['follow'].remove((payload['unfollow']))
+    user_id_to_unfollow = int(payload['unfollow'])
+    if user_id not in app.users or user_id_to_unfollow not in app.users:
+        return '사용자가 존재하지 않습니다.', 400
+    user = app.users[user_id]
+    user.setdefault('follow', set()).discard(user_id_to_unfollow)
+    return jsonify(user)
